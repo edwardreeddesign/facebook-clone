4fe561a923c2adf34ce540fcbe12cea640c48931
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-08-25 07:43:45 -0400

    added header component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [
+      'links.papareact.com',
+      'platform-lookaside.fbsbx.com',
+      'firebasestorage.googleapis.com',
+      'www.edwardreeddesigns.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
